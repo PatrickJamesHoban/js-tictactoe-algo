b6355bf81fc648cb7e820e1c02dac3808a00d6c9
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-10-18 19:45:56 -0500

    Added arrays to use for test cases and initial driver code

--- tic_tac_toe_complete.js
+++ tic_tac_toe_complete.js
@@ -1,5 +1,34 @@
 // PSEUDO CODE
 
 // CODE
+function isSolved(board) {
+  // TODO: Check if the board is solved!
+}
 
 // DRIVER CODE / TESTS
+
+// Unsolved and unfinished.
+a1 = [[0, 0, 1], [0, 1, 2], [2, 1, 0]]; // expect -1 = unsolved and unfinished return
+
+// X win horizontal
+a2 = [[1, 1, 1], [2, 0, 2], [0, 0, 0]];
+
+// X win vertical
+a3 = [[1, 1, 2], [2, 1, 2], [0, 1, 0]];
+
+// X win diagonal
+a4 = [[1, 1, 2], [2, 1, 2], [0, 2, 1]];
+
+// O win horizontal
+a5 = [[1, 1, 2], [2, 2, 2], [1, 0, 1]];
+
+// O win vertical
+a6 = [[1, 1, 2], [0, 2, 2], [1, 1, 2]];
+
+// O win diagonal
+a7 = [[1, 1, 2], [2, 2, 1], [2, 0, 1]];
+
+// Game is finished and is a draw.  No zeros remaining.
+a5 = [[1, 1, 2], [2, 2, 1], [1, 2, 1]];
+
+// Bonus zeros remaining cannot create a win for either X or O.
